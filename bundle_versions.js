30f0d580cf4ccf5ac6c25bf7047edbbc8a384f1a
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2013-07-05 15:01:09 +0100

    bundle_versions.js: update bundles

--- bundle_versions.js
+++ bundle_versions.js
@@ -1,4 +1,4 @@
 // Bundle versions
 module.exports.libVersion = '825e72ccbb773678ead1e3f61841310d';
-module.exports.appVersion = 'f9c508e1a8ed1962aec94be1709f4cf3';
+module.exports.appVersion = '91c14c8779a42df6c0c3c68b00ccdd0c';
 module.exports.cssVersion = 'c5248eee92323d5b65dbd4912f874bc9';
